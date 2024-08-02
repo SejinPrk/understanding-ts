5891e22773744f0d408a3983040ad506a44a046e
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-02 14:43:25 +0900

    js vs. ts

--- app.ts
+++ app.ts
@@ -1,12 +1,20 @@
-function add(n1: number, n2: number) {
-  // if (typeof n1 === 'number' || typeof n2 === 'number'){
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  // if (typeof n1 !== 'number' || typeof n2 !== 'number'){
   //   throw new Error('Incorrect input!');
   // }
-  return n1 + n2;
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return n1 + n2;
+  }
 }
 
-const number1 = 5; // = 5.0
+let number1: number; // 나중에 변수에 값을 넣을 경우를 위해서만 여기서 타입 지정
+number1 = 5; // = 0.5
 const number2 = 2.8;
+const printResult = true;
+let resultPhraes = 'Result is: ';
+// resultPhraes = 0; // 불가능
 
-const result = add(number1, number2);
-console.log(result);
+add(number1, number2, printResult, resultPhraes);
