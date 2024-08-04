d520de2e635d7a4188734357d3e3397aad401925
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-04 19:29:18 +0900

    array

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
 var person = {
-    // const person = {
     name: 'Sejin',
-    age: 23
+    age: 23,
+    hobbies: ['Sports', 'Cooking']
 };
+var favoriteActivites;
+favoriteActivites = ['Sports'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -1,10 +1,19 @@
-const person: {
-  name: string;
-  age: number;
-} = {
-// const person = {
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
   name: 'Sejin',
-  age: 23
+  age: 23,
+  hobbies: ['Sports', 'Cooking']
 };
 
+let favoriteActivites: string[];
+favoriteActivites = ['Sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map()); // !!! ERROR !!!
+}
