aa3ecc93f011694f72863a2833e0610aaf6532e2
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-04 19:10:53 +0900

    object types

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
-function add(n1, n2, showResult, phrase) {
-    // if (typeof n1 !== 'number' || typeof n2 !== 'number'){
-    //   throw new Error('Incorrect input!');
-    // }
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return n1 + n2;
-    }
-}
-var number1 = 5; // = 5.0
-var number2 = 2.8;
-var printResult = true;
-var resultPhraes = 'Result is: ';
-add(number1, number2, printResult, resultPhraes);
+var person = {
+    // const person = {
+    name: 'Sejin',
+    age: 23
+};
+console.log(person.name);
