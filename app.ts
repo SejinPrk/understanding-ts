5fc1c4d9ca584fa49a5fc38138c5d3e8308a8719
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-05 13:15:35 +0900

    Function types & Callbacks

--- app.ts
+++ app.ts
@@ -6,6 +6,11 @@ function printResult(num : number): void {
   console.log('Result: ', num);
 }
 
+function addHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(5, 12));
 
 let combineValues: (a: number, b: number) => number;
@@ -17,3 +22,7 @@ combineValues = add;
 console.log(combineValues(8, 8));
 
 // let someValue: undefined;
+
+addHandle(10, 20, (result) => {
+  console.log(result);
+});
