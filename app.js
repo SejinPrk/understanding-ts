3c4d726ce880c96a1d0fc3345f3b18ee24b5e907
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-05 13:01:12 +0900

    return types: undefined vs. void

--- app.js
+++ app.js
@@ -1,23 +1,7 @@
-// union type
-function combine(input1, input2, resultConversion) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2; // +: converted to a number
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-    // if (resultConversion === 'as-number') {
-    //   // return parseFloat(result); 아래와 동일
-    //   return +result;
-    // } else {
-    //   return result.toString();
-    // }
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
-var combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
-var combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+function printResult(num) {
+    console.log('Result: ', num);
+}
+console.log(printResult(add(5, 12)));
