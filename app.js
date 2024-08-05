f76483bb03d3f632ceaa62902c104b22578356ab
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-05 10:48:26 +0900

    tuple

--- app.js
+++ app.js
@@ -5,12 +5,16 @@
 var person = {
     name: 'Sejin',
     age: 23,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 };
+person.role.push('admin');
+person.role[1] = 10;
 var favoriteActivites;
 favoriteActivites = ['Sports'];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+    // console.log(hobby.map()); // !!! ERROR !!!
 }
--- app.ts
+++ app.ts
@@ -2,11 +2,23 @@
 //   name: string;
 //   age: number;
 // } = {
-const person = {
+const person: {
+  name: string; // string
+  age: number; // int
+  hobbies: string[]; // array
+  role: [number, string]; // tuple type
+} = {
   name: 'Sejin',
   age: 23,
-  hobbies: ['Sports', 'Cooking']
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author']
 };
+
+
+// person.role = [0, 'admin', 'user']; // 값을 2개만 허용했으므로 오류 발생
+// person.role.push('admin'); // 여기서는 오류가 나지 않는 이유?: push는 튜플에서 예외적으로 허용
+// person.role[1] = 10; // 2번째 value로 string이 들어오지 않았기 때문에 오류 발생
+
 
 let favoriteActivites: string[];
 favoriteActivites = ['Sports'];
