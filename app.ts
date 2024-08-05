5c2d83f29fd5f87a91f7070fac5a9f9cb0a3b7a5
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-05 13:10:07 +0900

    function as  types

--- app.ts
+++ app.ts
@@ -4,9 +4,16 @@ function add(n1: number, n2: number) {
 
 function printResult(num : number): void {
   console.log('Result: ', num);
-  return;
 }
 
 printResult(add(5, 12));
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+
+console.log(combineValues(8, 8));
 
 // let someValue: undefined;
