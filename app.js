c3f9062d7e75012e7dd89507bb15e8227de7d55f
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-06 15:38:00 +0900

    tsconfig options: exclude, include, files

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-// unknown: 어떤 타입을 지정할지 모르겠을 때 사용. 
-var userInput; // unknown: 어떤 변수를 넣어도 오류가 나지 않음
-var userName;
-userInput = 5;
-userInput = 'Sejin';
-if (typeof userInput === 'string') { // type check를 통해 오류 해결
-    userName = userInput;
-}
-// userName = userInput; // unknown을 string으로 할당 불가. 오류!! any로 변경 시 오류 해결
-// unknown이 any보다 더 엄격함
-// never
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-var result = generateError('An error occured!', 500);
-console.log(result);
+"use strict";
+const myName = 'Sejin';
+console.log(myName);
---
+++ basic-types/basics.js
@@ -0,0 +1,20 @@
+// "use strict";
+// function add(n1, n2, showResult, phrase) {
+//     // if (typeof n1 !== 'number' || typeof n2 !== 'number'){
+//     //   throw new Error('Incorrect input!');
+//     // }
+//     const result = n1 + n2;
+//     if (showResult) {
+//         console.log(phrase + result);
+//     }
+//     else {
+//         return n1 + n2;
+//     }
+// }
+// let number1; // 나중에 변수에 값을 넣을 경우를 위해서만 여기서 타입 지정
+// number1 = 5; // = 0.5
+// const number2 = 2.8;
+// const printResult = true;
+// let resultPhraes = 'Result is: ';
+// // resultPhraes = 0; // 불가능
+// add(number1, number2, printResult, resultPhraes);
