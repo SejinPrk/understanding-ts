efacb8bc4cec6eede4a3ea7a2770f683c91907d2
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-06 15:12:42 +0900

    never

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
-function add(n1, n2) {
-    return n1 + n2;
+// unknown: 어떤 타입을 지정할지 모르겠을 때 사용. 
+var userInput; // unknown: 어떤 변수를 넣어도 오류가 나지 않음
+var userName;
+userInput = 5;
+userInput = 'Sejin';
+if (typeof userInput === 'string') { // type check를 통해 오류 해결
+    userName = userInput;
 }
-function printResult(num) {
-    console.log('Result: ', num);
+// userName = userInput; // unknown을 string으로 할당 불가. 오류!! any로 변경 시 오류 해결
+// unknown이 any보다 더 엄격함
+// never
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
-function addHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(5, 12));
-var combineValues;
-combineValues = add;
-// combineValues = printResult;
-// combineValues = 5;
-console.log(combineValues(8, 8));
-// let someValue: undefined;
-addHandle(10, 20, function (result) {
-    console.log(result);
-});
+var result = generateError('An error occured!', 500);
+console.log(result);
