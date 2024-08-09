891aa9998ca8c5b6254f9847858ad5ba74300b51
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-09 15:24:38 +0900

    0809

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,18 @@
+let appId = 'abc';
 const button = document.querySelector('button')!;
 
-button.addEventListener('click', () => {
-  console.log('Clicked!');
-});
+function add (n1: number, n2: number) {
+  if (n1 + n2 > 0) {
+    return n1 + n2;
+  }
+  return;
+}
 
-// comment
-const map = new Map();
+function clickHandler(message: string) {
+  // let userName = 'Max';
+  console.log('Clicked' + message);
+}
+// a comment
+if (button) {
+  button.addEventListener('click', clickHandler.bind(null, "You are welcome!"));
+}
