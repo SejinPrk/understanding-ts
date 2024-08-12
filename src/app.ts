675f959ee64b5dbf8070663ac552d2df4a49f0bf
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-08-12 22:51:58 +0900

    let&const

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,17 @@
-let appId = 'abc';
-const button = document.querySelector('button')!;
+const userName = 'Sejin';
+// userName  = 'SejinPark';
+let age = 23;
 
-function add (n1: number, n2: number) {
-  if (n1 + n2 > 0) {
-    return n1 + n2;
-  }
-  return;
+age = 22; 
+
+function add(a:number, b:number) {
+  let result;
+  result = a + b;
+  return result;
 }
 
-function clickHandler(message: string) {
-  // let userName = 'Max';
-  console.log('Clicked' + message);
+if (age > 20) {
+  var isOld = true;
 }
-// a comment
-if (button) {
-  button.addEventListener('click', clickHandler.bind(null, "You are welcome!"));
-}
+
+// console.log(isOld); // var이 for문 내부에 있기 때문에 에러 발생
